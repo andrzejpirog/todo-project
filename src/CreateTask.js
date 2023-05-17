8e23505456fb69e8fd3315da4cfab35607ff1786
Andrzej Piróg <andrzej.pirog@yahoo.com>
2023-05-17 09:40:37 +0200

    initial files

---
+++ src/CreateTask.js
@@ -0,0 +1,11 @@
+import "bulma/css/bulma.min.css";
+function CreateTask() {
+  return (
+    <div classname=".container.is-max-widescreen">
+      <h1>Zadanie</h1>
+      <button></button>
+    </div>
+  );
+}
+
+export default CreateTask;
